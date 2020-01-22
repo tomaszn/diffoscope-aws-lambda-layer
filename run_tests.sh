9bbfb18ed8b4e899d35ff7d7a1b10fba11c955fe
Tomasz Nowak <tomaszn@users.noreply.github.com>
2020-01-22 12:30:32 +0100

    Extract tests from workflow

---
+++ run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+RUNTIME=${1:-python3.6}
+
+rm -r opt
+mkdir opt
+cd opt
+unzip ../layer_diffoscope_$RUNTIME.zip
+ldd bin/* || true
+ldd lib/* || true
+cd ..
+ls -l
+docker run -e DOCKER_LAMBDA_DEBUG=1 --rm -v "$PWD":/var/task:ro,delegated -v "$PWD"/opt/:/opt:ro,delegated lambci/lambda:$RUNTIME tests.run 2> /tmp/test_results
+cat /tmp/test_results
+grep '^OK$' /tmp/test_results
